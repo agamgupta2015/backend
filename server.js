ec2947c7f40e37816dbf9885d204ea481b8edb71
agamgupta2015 <agamgupta2015@gmail.com>
2022-12-22 16:29:43 +0530

    added 404 route not found middleware

--- server.js
+++ server.js
@@ -9,9 +9,18 @@ const HOST = process.env.host || "localhost";
 
 Database();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.get("/api/health",(req,res)=>{
     res.send("Server is Runing fine")
 })
+
+app.use((req, res, next) =>
+    res.status(404).send("You are looking for something that we not have!")
+);
+
+app.use((err, req, res, next) => res.status(500).send("Something went wrong!"));
 
 app.listen(PORT, () => {
     console.log(` Shuffle app listening at http://${HOST}:${PORT}`);
