eb1beb6f3d15954418b122ab348d7689e9fa4297
agamgupta2015 <agamgupta2015@gmail.com>
2022-12-22 15:53:27 +0530

    dded insert a image with details api

--- routes/admin.js
+++ routes/admin.js
@@ -19,4 +19,28 @@ route.get("/add-category/:categoryName", async (req, res, next) => {
     }
 });
 
+route.post("/add-image", async (req, res, next) => {
+    try {
+        const name = req.body.name;
+        const category = req.body.category;
+        const imageUrl = req.body.imageUrl;
+
+        if (!name || !category.length || !imageUrl) {
+            res.status(400).send("Bad Request");
+        }
+
+        const newGalleryData = {
+            name: name,
+            category: category,
+            imageUrl: imageUrl,
+        };
+
+        await GalleryModel.create(newGalleryData);
+        res.send("Image added successfully!");
+    } catch (error) {
+        console.log(error);
+        next(error);
+    }
+});
+
 module.exports = route;
